664f5a45a10377357f66f919ac9229dcd890d63b
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-12-27 21:27:49 +0700

    sign up form spec

--- spec/web/views/users/new_spec.rb
+++ spec/web/views/users/new_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 require_relative '../../../../apps/web/views/users/new'
 
 describe Web::Views::Users::New do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) { Hash[params: {}] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/users/new.html.erb') }
   let(:view)      { Web::Views::Users::New.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    view.foo.must_equal exposures.fetch(:foo)
+  it 'displays sign up form' do
+    rendered.must_match('user[email]')
+    rendered.must_match('user[password]')
+    rendered.must_match('user[confirm_password]')
   end
 end

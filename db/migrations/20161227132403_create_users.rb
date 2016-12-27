67e5ba2c2d97d01e62dcf81da5bd438aa420433d
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-12-27 20:37:35 +0700

    create Users table

---
+++ db/migrations/20161227132403_create_users.rb
@@ -0,0 +1,14 @@
+Hanami::Model.migration do
+  change do
+    create_table :users do
+      primary_key :id
+
+      column :email,  String, null: false
+      column :password_hash, String, null: false
+      column :password_salt, String, null: false
+
+      column :created_at, DateTime, null: false
+      column :updated_at, DateTime, null: false
+    end
+  end
+end

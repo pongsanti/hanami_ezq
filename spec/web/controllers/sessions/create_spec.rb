4b7d533a0038b78dcb6fde3c4898e6bdcc819eef
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-12-30 01:06:13 +0700

    temporary fix test by mocking warden methods

--- spec/web/controllers/sessions/create_spec.rb
+++ spec/web/controllers/sessions/create_spec.rb
@@ -1,32 +1,32 @@
 require 'spec_helper'
 require_relative '../../../../apps/web/controllers/sessions/create'
 
+# mock warden
+class MyWarden
+  attr_accessor :count
+  def initialize
+    self.count = 0
+  end
+
+  def authenticate!
+    self.count = self.count + 1
+  end
+end
+
+myWarden = MyWarden.new
+
 describe Web::Controllers::Sessions::Create do
   let(:action) { Web::Controllers::Sessions::Create.new }
 
   describe 'with valid params' do
-    let(:params) { Hash[session: { email: 'johny@gmail.com', password: '1234abcd' }] }
-
-    before do
-      UserRepository.new.clear
-      # create a user in database
-      user_create_action = Web::Controllers::Users::Create.new
-      local_params = Hash[user: { email: params[:session][:email], 
-        password: params[:session][:password],
-        password_confirmation: params[:session][:password] }]
-      user_create_action.call(local_params)
-
-      user = UserRepository.new.by_email('johny@gmail.com')[0]
-    end
+    let(:params) { Hash[session: { email: 'johny@gmail.com', password: '1234abcd' },
+    'warden' => myWarden ] }
 
     it 'logs user in' do
 
       response = action.call(params)
       
-      user_id = action.session[:user_id]
-      user_id.wont_be_nil
-      (UserRepository.new.find(user_id)).email.must_equal params[:session][:email]
-
+      myWarden.count.must_equal 1
       response[0].must_equal 302
       response[1]['Location'].must_equal '/auth'
     end

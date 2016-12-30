86fc0fd693abc93d8d507fd6b67fc695fcbe0ea5
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-12-30 23:24:01 +0700

    sessions destroy action - remove unused template - eliminate MyWarden class name duplicate across tests

--- spec/web/controllers/sessions/destroy_spec.rb
+++ spec/web/controllers/sessions/destroy_spec.rb
@@ -1,27 +1,21 @@
 require 'spec_helper'
 require_relative '../../../../apps/web/controllers/sessions/destroy'
 
-class MyWarden
-  attr_accessor :count
-  def initialize
-    self.count = 0
-  end
-  def logout
-    self.count = self.count + 1
-  end
+warden = Object.new
+warden.instance_eval do
+  def count; @count; end
+  def logout; @count = 1; end
 end
-
-myWarden = MyWarden.new
 
 describe Web::Controllers::Sessions::Destroy do
   let(:action) { Web::Controllers::Sessions::Destroy.new }
-  let(:params) { Hash[session: {}, 'warden' => myWarden ] }
+  let(:params) { Hash[session: {}, 'warden' => warden ] }
 
   it 'is successful' do
-    myWarden.count.must_equal 0
+    warden.count.must_equal nil
 
     response = action.call(params)
-    myWarden.count.must_equal 1
+    warden.count.must_equal 1
     response[0].must_equal 302
     response[1]['Location'].must_equal '/sessions/new'
   end

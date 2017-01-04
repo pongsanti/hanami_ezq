f0b97ec36cfe13910f1bf4aedccd2469741a2bab
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-01-04 21:05:42 +0700

    decrypt 'rack.session' cookie

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 //var http = require('http')
+require('dotenv').config({path: '.env.development'})
+let decode = require('./ws/racksession/decoder')
+
 
 var Server = require('socket.io')
+var Cookie = require('cookie');
 var io = new Server(3000)
 
 io.on('connection', function (socket) {
+  secret = process.env.WEB_SESSIONS_SECRET
+  console.log(secret)
+  
   console.log(`a user connected: ${socket.id}`)
 
+  let cookie = socket.handshake.headers['cookie'];
+  console.log(cookie)
+  let parseCookie = Cookie.parse(cookie);
+  let rackCookie = parseCookie['rack.session']
+  console.log(rackCookie);
+
+  decode_args = {secret: process.env.WEB_SESSIONS_SECRET, cookie: rackCookie };
+  decode(decode_args, function (err, data) {
+    console.log(data);
+    console.log(data['warden.user.default.key'])
+  });
 
   socket.on('disconnect', function () {
     console.log('user disconnected')

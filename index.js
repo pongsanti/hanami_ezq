af07c1184dde9e023256d113d50b576cca043d38
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-01-06 00:20:03 +0700

    following js standard

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
-//var http = require('http')
+// var http = require('http')
 require('dotenv').config({path: '.env.development'})
-var unescape = require('./ws/unescape');
-var cookieParser = require('./ws/cookie_parser');
-var decode = require('./ws/decoder');
+var unescape = require('./ws/unescape')
+var cookieParser = require('./ws/cookie_parser')
+var decode = require('./ws/decoder')
 
-var Server = require('socket.io');
-var io = new Server(3000);
+var Server = require('socket.io')
+var io = new Server(3000)
 
 io.on('connection', function (socket) {
-  console.log(`a user connected: ${socket.id}`);
-  secret = process.env.WEB_SESSIONS_SECRET;
+  console.log(`a user connected: ${socket.id}`)
+  let secret = process.env.WEB_SESSIONS_SECRET
 
-  let cookie = socket.handshake.headers['cookie'];
-  let parseCookie = cookieParser(unescape(cookie));
+  let cookie = socket.handshake.headers['cookie']
+  let parseCookie = cookieParser(unescape(cookie))
   let rackCookie = parseCookie['rack.session']
 
-  let [b64Value, hexDigest] = decode.splitRackCookie(rackCookie);
+  let [b64Value, hexDigest] = decode.splitRackCookie(rackCookie)
   if (hexDigest === decode.hmac(secret, b64Value)) {
-    rackSessionObj = decode.marshal(b64Value);
+    let rackSessionObj = decode.marshal(b64Value)
     if (rackSessionObj['warden.user.default.key']) {
-      let roomNum = rackSessionObj['warden.user.default.key'];
-      socket.roomNum = roomNum;
-      console.log(`A user id: ${socket.id} is joining room: ${roomNum}`);
-      
-      socket.join(roomNum);
-      io.to(socket.roomNum).emit('queue update', "Hello number 5");
+      let roomNum = rackSessionObj['warden.user.default.key']
+      socket.roomNum = roomNum
+      console.log(`A user id: ${socket.id} is joining room: ${roomNum}`)
 
+      socket.join(roomNum)
+      io.to(socket.roomNum).emit('queue update', 'Hello number 5')
     } else {
-      console.log('session invalid');
+      console.log('session invalid')
     }
   } else {
-    console.log('session invalid');
+    console.log('session invalid')
   }
 
   socket.on('disconnect', function () {
-    console.log(`A user ${socket.id} has disconnected`);
+    console.log(`A user ${socket.id} has disconnected`)
+    console.log('----------')
   })
 
   socket.on('request ticket', function (msg) {
     if (socket.roomNum) {
-      io.to(socket.roomNum).emit('queue update', msg);
+      io.to(socket.roomNum).emit('queue update', msg)
     }
-  });
+  })
+})
 
-});
-
-console.log('listening on *:3000');
+console.log('listening on *:3000')

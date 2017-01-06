e4d6a55cd545e8c05cb9d3faefb195ae7e8f4f1a
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-01-06 21:23:03 +0700

    channel subscriber test cases

--- test/channel_subscriber_test.js
+++ test/channel_subscriber_test.js
@@ -1,10 +1,33 @@
 require('chai').should()
-let rackSessionParser = require('./../ws/channel_subscriber')
+let expect = require('chai').expect
+let channel_subscriber = require('./../ws/channel_subscriber')
+
+const connectionString = 'postgres://dev:dev@localhost/ezq_dev'
 
 describe('channel subscriber', function () {
   it('should create new object with function constructor', function () {
-    let parser = rackSessionParser.create('connString', 'queue update')
-    parser.should.have.property('connectionString').with.equal('connString')
-    parser.should.have.property('channelName').with.equal('queue update')
+    let subscriber = channel_subscriber.create('connString', 'queue update')
+    subscriber.should.have.property('connectionString').with.equal('connString')
+    subscriber.should.have.property('channelName').with.equal('queue update')
   })
+
+  it('should connect to database successfully', function (done) {
+    let subscriber = channel_subscriber.create(connectionString, 'queue update')
+    subscriber.connect(function (err) {
+      expect(err).to.be.null
+      subscriber.should.have.property('client')
+      done()
+    })
+  })
+
+  it('should return error if connection failure', function (done) {
+    let subscriber = channel_subscriber.create('invalid connection string', 'queue update')
+    subscriber.connect(function (err) {
+      err.should.be.an('error')
+      subscriber.should.not.have.property('client')
+      done()
+    })
+  })
+
+  it('should listen to channel name')
 })
--- ws/channel_subscriber.js
+++ ws/channel_subscriber.js
@@ -13,12 +13,38 @@ Procedures
   3. Send data through callback when notification event occurs.
 */
 
+const pg = require('pg')
+
 let ChannelSubscriber = {
   create: function (connectionString, channelName) {
     let self = Object.create(this)
     self.connectionString = connectionString
     self.channelName = channelName
     return self
+  },
+
+  connect: function (callback) {
+    console.log('connect')
+    let self = this
+    pg.connect(this.connectionString, function (err, client) {
+      if (err) {
+        callback(new Error('Cannot connect to database.'))
+      } else {
+        self.client = client
+        callback(null)
+      }
+    })
+  },
+
+  listen: function (callback) {
+    this.client.on('notification', function (msg) {
+      callback(msg)
+    })
+    this.client.query(`LISTEN ${this.channelName}`)
+  },
+
+  end: function () {
+    this.client.end()
   }
 }
 

e398541f040a2518360e099d4824d90b14a18cbe
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-01-08 20:06:23 +0700

    also plays sound for other panel

--- apps/web/assets/javascripts/queue_info.js
+++ apps/web/assets/javascripts/queue_info.js
@@ -1,10 +1,11 @@
 var socket = io()
 
-$(function() {
+$(function () {
   socket.on('connect', function (data) {
   })
 
-  socket.on('queue update', function(msg){
+  socket.on('queue update', function (msg) {
+    audio.playSoundFiles(msg)
     $('h1.current_queue').html(msg)
   })
 })
--- apps/web/assets/javascripts/queue_ticket.js
+++ apps/web/assets/javascripts/queue_ticket.js
@@ -1,14 +1,15 @@
 var socket = io()
 
-$(function() {
+$(function () {
   socket.on('connect', function (data) {
   })
 
-  socket.on('queue update', function(msg){
+  socket.on('queue update', function (msg) {
+    audio.playSoundFiles(msg)
     $('h1.current_queue').html(msg)
   })
 
-  socket.on('ticket update', function(msg){
+  socket.on('ticket update', function (msg) {
     $('h1.ticket_queue').html(msg)
   })
 

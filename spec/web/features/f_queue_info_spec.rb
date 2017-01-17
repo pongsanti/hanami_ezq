b0343d4134ddcbcd0d07b0751c9d105ac0ba62f5
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-01-17 10:57:30 +0700

    add test cases for non logged-in user

--- spec/web/features/f_queue_info_spec.rb
+++ spec/web/features/f_queue_info_spec.rb
@@ -1,24 +1,34 @@
 require 'features_helper'
 
 describe 'Visit queue info page' do
-  
-  queue_number = 5
 
-  before do
-    login_as UserRepository.new.create({email: 'john@gmail.com', 
-      password_hash: 'hash', queue_number: queue_number})
+  describe 'with non logged-in user' do
+    it 'shows log in page' do
+      visit '/auth/queueinfo'
+
+      page.body.must_include 'Log in'
+    end
   end
-
-  after do
-    Warden.test_reset!
-    UserRepository.new.clear
-  end  
   
-  it 'shows current Q number' do
-    visit '/auth/queueinfo'
+  describe 'with logged-in user' do
+    queue_number = 5
 
-    page.body.must_include('Queue Info')
-    page.has_css?('h1.current_queue', text: queue_number).must_equal true
-    page.has_css?('input[type="checkbox"].audio-toggle').must_equal true
+    before do
+      login_as UserRepository.new.create({email: 'john@gmail.com', 
+        password_hash: 'hash', queue_number: queue_number})
+    end
+
+    after do
+      Warden.test_reset!
+      UserRepository.new.clear
+    end  
+    
+    it 'shows current Q number' do
+      visit '/auth/queueinfo'
+
+      page.body.must_include('Queue Info')
+      page.has_css?('h1.current_queue', text: queue_number).must_equal true
+      page.has_css?('input[type="checkbox"].audio-toggle').must_equal true
+    end
   end
 end
--- spec/web/features/f_queue_ticket_spec.rb
+++ spec/web/features/f_queue_ticket_spec.rb
@@ -2,28 +2,39 @@ require 'features_helper'
 
 describe 'Visit queue info page' do
 
-  queue_number = 5
-  ticket_number = 8
-  
-  before do
-    login_as UserRepository.new.create({email: 'john@gmail.com',
-      password_hash: 'hash',
-      queue_number: queue_number,
-      ticket_number: ticket_number})
+  describe 'with non logged-in user' do
+    it 'shows log in page' do
+      visit '/auth/queueticket'
+
+      page.body.must_include 'Log in'
+    end
   end
 
-  after do
-    Warden.test_reset!
-    UserRepository.new.clear
-  end  
-  
-  it 'shows current Q number' do
-    visit '/auth/queueticket'
+  describe 'with logged-in user' do
 
-    page.body.must_include('Queue Ticket')
-    page.has_css?('h1.current_queue', text: queue_number).must_equal true
-    page.has_css?('h1.ticket_queue', text: ticket_number).must_equal true
-    page.has_css?('button.request_ticket').must_equal true
-    page.has_css?('input[type="checkbox"].audio-toggle').must_equal true    
+    queue_number = 5
+    ticket_number = 8
+    
+    before do
+      login_as UserRepository.new.create({email: 'john@gmail.com',
+        password_hash: 'hash',
+        queue_number: queue_number,
+        ticket_number: ticket_number})
+    end
+
+    after do
+      Warden.test_reset!
+      UserRepository.new.clear
+    end  
+    
+    it 'shows current Q number' do
+      visit '/auth/queueticket'
+
+      page.body.must_include('Queue Ticket')
+      page.has_css?('h1.current_queue', text: queue_number).must_equal true
+      page.has_css?('h1.ticket_queue', text: ticket_number).must_equal true
+      page.has_css?('button.request_ticket').must_equal true
+      page.has_css?('input[type="checkbox"].audio-toggle').must_equal true    
+    end
   end
 end
